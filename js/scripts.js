469817abc79231ae31adfe117bcf8eabfd0a2533
Nick Sullivan <nicholassull@gmail.com>
2021-11-30 19:36:08 -0800

    add prototype for Account to deposit funds

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@ function Account(balance, name) {
   this.balance = balance;
   this.name = name;
 }
+
+Account.prototype.deposit = function(funds) {
+  this.balance += funds;
+  return this.balance;
+}
